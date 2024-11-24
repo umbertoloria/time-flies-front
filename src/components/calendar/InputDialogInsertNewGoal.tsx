1778f8129a847da88cc3ececf053c23efb381879
Umberto Loria <umberto.loria@gmail.com>
2024-11-24 18:54:53 +0100

    GenericDialog used for InputDialogSeeNotes

--- src/components/calendar/InputDialogInsertNewGoal.tsx
+++ src/components/calendar/InputDialogInsertNewGoal.tsx
@@ -8,35 +8,33 @@ export const InputDialogInsertNewGoal: FC = () => {
 
   return (
     <>
-      <div>
-        {isOpen && (
-          <GenericDialog
-            onClose={closeDialog}
-            labelOnClose='Indietro'
-            title='Segna come fatto'
-          >
-            <div className='p-4'>
-              Confermi di aver svolto questa attività?
-              {/*<input
-                type='text'
-                id='input-field'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900'
-                value={inputValue}
-                onChange={handleInputChange}
-              />*/}
-            </div>
-            <div className='flex items-center justify-end p-4 border-t border-gray-200 rounded-b dark:border-gray-600'>
-              <button
-                type='button'
-                className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-                onClick={confirmProgressDone}
-              >
-                Conferma
-              </button>
-            </div>
-          </GenericDialog>
-        )}
-      </div>
+      {isOpen && (
+        <GenericDialog
+          onClose={closeDialog}
+          labelOnClose='Indietro'
+          title='Segna come fatto'
+        >
+          <div className='p-4'>
+            Confermi di aver svolto questa attività?
+            {/*<input
+              type='text'
+              id='input-field'
+              className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900'
+              value={inputValue}
+              onChange={handleInputChange}
+            />*/}
+          </div>
+          <div className='flex items-center justify-end p-4 border-t border-gray-200 rounded-b dark:border-gray-600'>
+            <button
+              type='button'
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              onClick={confirmProgressDone}
+            >
+              Conferma
+            </button>
+          </div>
+        </GenericDialog>
+      )}
     </>
   )
 }
