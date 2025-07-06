bb92b3adf2e5b73e78d3b5cda573d25bfceea778
Umberto Loria <umberto.loria@gmail.com>
2025-07-06 13:06:38 +0200

    NotesFieldEditor extracted

--- src/context/dialog-insert-new-goal/DialogForInsertNewGoal.tsx
+++ src/context/dialog-insert-new-goal/DialogForInsertNewGoal.tsx
@@ -12,7 +12,7 @@ export const DialogForInsertNewGoal: FC = () => {
   const [enableNoteInput, setEnableNoteInput] = useState(INITIAL_ENABLE_NOTE)
   const [inputValue, setInputValue] = useState(INITIAL_INPUT_VALUE)
   useEffect(() => {
-    if (isOpen) {
+    if (!isOpen) {
       setEnableNoteInput(INITIAL_ENABLE_NOTE)
       setInputValue(INITIAL_INPUT_VALUE)
     }
@@ -28,35 +28,12 @@ export const DialogForInsertNewGoal: FC = () => {
         >
           <div className='p-4'>
             <p>{'Confermi di aver svolto questa attività?'}</p>
-            <div className='pt-2'>
-              {'Inserisci note'}
-              <input
-                type='checkbox'
-                className='inline ml-1'
-                checked={enableNoteInput}
-                onChange={event => {
-                  setEnableNoteInput(event.currentTarget.checked)
-                }}
-              />
-              <input
-                type='text'
-                id='input-field'
-                className={classNames(
-                  'block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900',
-                  {
-                    'bg-white': enableNoteInput,
-                    'bg-gray-100': !enableNoteInput,
-                    'text-gray-700': enableNoteInput,
-                    'text-gray-500': !enableNoteInput,
-                  }
-                )}
-                value={inputValue}
-                disabled={!enableNoteInput}
-                onChange={event => {
-                  setInputValue(event.target.value)
-                }}
-              />
-            </div>
+            <NotesFieldEditor
+              enableNoteInput={enableNoteInput}
+              setEnableNoteInput={setEnableNoteInput}
+              inputValue={inputValue}
+              setInputValue={setInputValue}
+            />
           </div>
           <div className='flex items-center justify-end p-4 border-t border-gray-200 rounded-b dark:border-gray-600'>
             <button
@@ -80,6 +57,47 @@ export const DialogForInsertNewGoal: FC = () => {
           </div>
         </GenericDialog>
       )}
+    </>
+  )
+}
+
+const NotesFieldEditor: FC<{
+  enableNoteInput: boolean
+  setEnableNoteInput: (enableNoteInput: boolean) => void
+  inputValue: string
+  setInputValue: (inputValue: string) => void
+}> = ({ enableNoteInput, setEnableNoteInput, inputValue, setInputValue }) => {
+  return (
+    <>
+      <div className='pt-2'>
+        {'Inserisci note'}
+        <input
+          type='checkbox'
+          className='inline ml-1'
+          checked={enableNoteInput}
+          onChange={event => {
+            setEnableNoteInput(event.currentTarget.checked)
+          }}
+        />
+        <input
+          type='text'
+          id='input-field'
+          className={classNames(
+            'block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900',
+            {
+              'bg-white': enableNoteInput,
+              'bg-gray-100': !enableNoteInput,
+              'text-gray-700': enableNoteInput,
+              'text-gray-500': !enableNoteInput,
+            }
+          )}
+          value={inputValue}
+          disabled={!enableNoteInput}
+          onChange={event => {
+            setInputValue(event.target.value)
+          }}
+        />
+      </div>
     </>
   )
 }
