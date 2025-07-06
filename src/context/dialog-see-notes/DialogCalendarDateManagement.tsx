9a337751f6e26ebb2baa27a3740c61e7a5514229
Umberto Loria <umberto.loria@gmail.com>
2025-07-06 09:31:45 +0200

    Dialog Calendar Date Management refactored

--- src/context/dialog-see-notes/DialogCalendarDateManagement.tsx
+++ src/context/dialog-see-notes/DialogCalendarDateManagement.tsx
@@ -43,6 +43,8 @@ export const DialogCalendarDateManagementInner: FC<{
     }
   }, [])
 
+  const editable = true // TODO: Understand when a Calendar Date can be updated
+
   return (
     <GenericDialog
       onClose={closeDialog}
@@ -64,26 +66,28 @@ export const DialogCalendarDateManagementInner: FC<{
               <Badge>Data</Badge>{' '}
               {displayDateFromLocalDate(data.data.date.date)}
             </p>
-            {!!data.data.date.notes && (
-              <>
-                <p>
-                  <Badge>Note</Badge>
-                </p>
-                <div>
+            <p>
+              <Badge>Note</Badge>
+            </p>
+            <div>
+              {data.data.date.notes ? (
+                <>
                   <CalendarDayNote
                     calendarId={data.data.calendar.id}
                     localDate={data.data.date.date}
                     notes={{
                       text: data.data.date.notes,
                     }}
-                    editable={true}
+                    editable={editable}
                     onUpdated={() => {
                       refreshDate()
                     }}
                   />
-                </div>
-              </>
-            )}
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
           </>
         )}
       </div>
@@ -103,71 +107,42 @@ const CalendarDayNote: FC<{
   const { updateCalendarDateNotes } = getSDK()
   const [editing, setEditing] = useState(false)
   const [loading, setLoading] = useState(false)
-  const inputRef = useRef<HTMLInputElement>(null)
 
   return (
     <div>
       {editing ? (
         <>
-          <form
-            onSubmit={e => {
-              e.preventDefault()
+          <NotesForm
+            initialValue={notes.text}
+            loading={loading}
+            editable={editable}
+            onSubmit={_notes => {
+              if (_notes === notes.text.trim()) {
+                // Useless to save: fake the update.
+                setEditing(false)
+                return
+              }
+              setLoading(true)
+              updateCalendarDateNotes(calendarId, localDate, _notes)
+                .then(() => {
+                  // Yay!
+
+                  fireEventCalendarUpdated({ calendarId })
+                  // fireEventStreamlineUpdated(undefined)
+                  // This wasn't a Planned Event.
+
+                  setLoading(false)
+                  setEditing(false)
+                  onUpdated()
+                })
+                .catch(err => {
+                  console.error(err)
+                  // TODO: Tell user all went KO
+                  alert('Errore avvenuto')
+                  setLoading(false)
+                })
             }}
-          >
-            <input
-              type='text'
-              name='notes-text'
-              ref={inputRef}
-              className='block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900 bg-white text-gray-700'
-              defaultValue={notes.text}
-              disabled={loading}
-            />
-            <button
-              className='btn-primary mt-1'
-              disabled={loading}
-              onClick={() => {
-                if (editable && !loading) {
-                  const inputElement = inputRef.current
-                  if (!inputElement) {
-                    return
-                  }
-                  const inputValue = inputElement.value
-                  // TODO: Duplicated code (*sdcn)
-                  const _notes = inputValue.trim()
-                  if (_notes.length < 2 || _notes.length > 300) {
-                    alert('Nota non valida: minimo 2 massimo 300 caratteri')
-                    return
-                  }
-                  if (_notes === notes.text.trim()) {
-                    // Useless to save: fake the update.
-                    setEditing(false)
-                    return
-                  }
-                  setLoading(true)
-                  updateCalendarDateNotes(calendarId, localDate, _notes)
-                    .then(() => {
-                      // Yay!
-
-                      fireEventCalendarUpdated({ calendarId })
-                      // fireEventStreamlineUpdated(undefined)
-                      // This wasn't a Planned Event.
-
-                      setLoading(false)
-                      setEditing(false)
-                      onUpdated()
-                    })
-                    .catch(err => {
-                      console.error(err)
-                      // TODO: Tell user all went KO
-                      alert('Errore avvenuto')
-                      setLoading(false)
-                    })
-                }
-              }}
-            >
-              {'Save'}
-            </button>
-          </form>
+          />
         </>
       ) : (
         <>
@@ -189,5 +164,54 @@ const CalendarDayNote: FC<{
         </>
       )}
     </div>
+  )
+}
+
+const NotesForm: FC<{
+  initialValue: string
+  loading: boolean
+  editable: boolean
+  onSubmit: (value: string) => void
+}> = ({ initialValue, loading, editable, onSubmit }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  return (
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+        }}
+      >
+        <input
+          type='text'
+          name='notes-text'
+          ref={inputRef}
+          className='block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900 bg-white text-gray-700'
+          defaultValue={initialValue}
+          disabled={loading}
+        />
+        <button
+          className='btn-primary mt-1'
+          disabled={loading}
+          onClick={() => {
+            if (editable && !loading) {
+              const inputElement = inputRef.current
+              if (!inputElement) {
+                return
+              }
+              const inputValue = inputElement.value
+              // TODO: Duplicated code (*sdcn)
+              const _notes = inputValue.trim()
+              if (_notes.length < 2 || _notes.length > 300) {
+                alert('Nota non valida: minimo 2 massimo 300 caratteri')
+                return
+              }
+              onSubmit(_notes)
+            }
+          }}
+        >
+          {'Save'}
+        </button>
+      </form>
+    </>
   )
 }
