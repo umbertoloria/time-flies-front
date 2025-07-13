ceb273adb3dc5bbd51cc9541f3ed1f189451eeb6
Umberto Loria <umberto.loria@gmail.com>
2025-07-13 22:55:44 +0200

    Diary components moved

---
+++ src/components/diary/Diary.tsx
@@ -0,0 +1,256 @@
+import { FC, useRef, useState } from 'react'
+import { TCalendarDate } from '../../remote/sdk/types'
+import { displayDateFromLocalDate } from '../calendar/utils.ts'
+import { Badge } from '../calendar/Badge.tsx'
+import { getSDK } from '../../remote/remote.ts'
+import { fireEventCalendarUpdated } from '../calendar/event-calendar-updated.ts'
+
+export const DiaryEntry: FC<{
+  data: TCalendarDate
+  refreshDate: () => void
+}> = ({ data, refreshDate }) => {
+  return (
+    <>
+      <i className='underline'>{displayDateFromLocalDate(data.date.date)}</i>
+      {data.calendar.usesNotes && (
+        <>
+          <CalendarDateNotesComponent
+            calendarDate={data}
+            refreshDate={refreshDate}
+          />
+        </>
+      )}
+    </>
+  )
+}
+
+const CalendarDateNotesComponent: FC<{
+  calendarDate: TCalendarDate
+  refreshDate: () => void
+}> = ({ calendarDate, refreshDate }) => {
+  return (
+    <>
+      <>
+        <p>
+          <Badge>Note</Badge>
+        </p>
+        <div>
+          {calendarDate.date.notes ? (
+            <>
+              <CalendarDayNoteSeeAndEdit
+                calendarId={calendarDate.calendar.id}
+                localDate={calendarDate.date.date}
+                notes={{
+                  text: calendarDate.date.notes,
+                }}
+                editable={true} // Notes are always editable.
+                onUpdated={() => {
+                  refreshDate()
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <NotesAddForm
+                calendarId={calendarDate.calendar.id}
+                localDate={calendarDate.date.date}
+                onInserted={() => {
+                  refreshDate()
+                }}
+              />
+            </>
+          )}
+        </div>
+      </>
+    </>
+  )
+}
+
+const CalendarDayNoteSeeAndEdit: FC<{
+  calendarId: number
+  localDate: string
+  notes: {
+    text: string
+  }
+  editable: boolean
+  onUpdated: () => void
+}> = ({ calendarId, localDate, notes, editable, onUpdated }) => {
+  const { updateCalendarDateNotes } = getSDK()
+  const [editing, setEditing] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <NotesForm
+            initialValue={notes.text}
+            loading={loading}
+            editable={editable}
+            onSubmit={_notes => {
+              if (!!_notes && _notes === notes.text.trim()) {
+                // Useless to save: fake the update.
+                setEditing(false)
+                return
+              }
+              setLoading(true)
+              updateCalendarDateNotes(
+                calendarId,
+                localDate,
+                _notes || undefined
+              )
+                .then(() => {
+                  // Yay!
+
+                  fireEventCalendarUpdated({ calendarId })
+                  // fireEventStreamlineUpdated(undefined)
+                  // This wasn't a Planned Event.
+
+                  setLoading(false)
+                  setEditing(false)
+                  onUpdated()
+                })
+                .catch(err => {
+                  console.error(err)
+                  // TODO: Tell user all went KO
+                  alert('Errore avvenuto')
+                  setLoading(false)
+                })
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <p>
+            {notes.text}
+            {editable && (
+              <button
+                className='btn-primary ml-2'
+                onClick={() => {
+                  if (editable) {
+                    setEditing(true)
+                  }
+                }}
+              >
+                {'Edit'}
+              </button>
+            )}
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
+
+const NotesAddForm: FC<{
+  calendarId: number
+  localDate: string
+  onInserted: () => void
+}> = ({ calendarId, localDate, onInserted }) => {
+  const { updateCalendarDateNotes } = getSDK()
+  const [adding, setAdding] = useState(false)
+  const [loading, setLoading] = useState(false)
+  return (
+    <>
+      {adding ? (
+        <>
+          <NotesForm
+            initialValue=''
+            loading={loading}
+            editable
+            onSubmit={_notes => {
+              setLoading(true)
+              updateCalendarDateNotes(
+                calendarId,
+                localDate,
+                _notes || undefined
+              )
+                .then(() => {
+                  // Yay!
+
+                  fireEventCalendarUpdated({ calendarId })
+                  // fireEventStreamlineUpdated(undefined)
+                  // This wasn't a Planned Event.
+
+                  setLoading(false)
+                  setAdding(false)
+                  onInserted()
+                })
+                .catch(err => {
+                  console.error(err)
+                  // TODO: Tell user all went KO
+                  alert('Errore avvenuto')
+                  setLoading(false)
+                })
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <button
+            className='btn-primary'
+            onClick={() => {
+              setAdding(true)
+            }}
+          >
+            {'Add'}
+          </button>
+        </>
+      )}
+    </>
+  )
+}
+
+const NotesForm: FC<{
+  initialValue: string
+  loading: boolean
+  editable: boolean
+  onSubmit: (value: string | null) => void
+}> = ({ initialValue, loading, editable, onSubmit }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  return (
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+        }}
+      >
+        <input
+          type='text'
+          name='notes-text'
+          ref={inputRef}
+          className='block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900 bg-white text-gray-700'
+          defaultValue={initialValue}
+          disabled={loading}
+        />
+        <button
+          className='btn-primary mt-1'
+          disabled={loading}
+          onClick={() => {
+            if (editable && !loading) {
+              const inputElement = inputRef.current
+              if (!inputElement) {
+                return
+              }
+              const inputValue = inputElement.value
+              if (inputValue === '') {
+                // Remove notes
+                onSubmit(null)
+              } else {
+                // TODO: Duplicated code (*sdcn)
+                const _notes = inputValue.trim()
+                if (_notes.length < 2 || _notes.length > 300) {
+                  alert('Nota non valida: minimo 2 massimo 300 caratteri')
+                  return
+                }
+                onSubmit(_notes)
+              }
+            }
+          }}
+        >
+          {'Save'}
+        </button>
+      </form>
+    </>
+  )
+}
--- src/context/dialog-date-panel/DialogDatePanel.tsx
+++ src/context/dialog-date-panel/DialogDatePanel.tsx
@@ -1,12 +1,11 @@
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, useEffect } from 'react'
 import { useDialogForDatePanel } from './ContextDialogForDatePanel.tsx'
 import { GenericDialog } from '../../components/calendar/GenericDialog.tsx'
-import { displayDateFromLocalDate } from '../../components/calendar/utils.ts'
 import { Badge } from '../../components/calendar/Badge.tsx'
 import { getSDK } from '../../remote/remote.ts'
-import { fireEventCalendarUpdated } from '../../components/calendar/event-calendar-updated.ts'
 import { useWrapperForCreateResource } from '../../lib/remote-resources.ts'
-import { TCalendar, TCalendarDate } from '../../remote/sdk/types'
+import { TCalendar } from '../../remote/sdk/types'
+import { DiaryEntry } from '../../components/diary/Diary.tsx'
 
 export const DialogDatePanel: FC = () => {
   const { isOpen, data, closeDialog } = useDialogForDatePanel()
@@ -89,7 +88,7 @@ const CalendarComponent: FC<{
     <>
       {calendar.days.map((day, index) => (
         <div key={index}>
-          <CalendarDateComponent
+          <DiaryEntry
             data={{
               calendar: {
                 id: calendar.id,
@@ -139,265 +138,14 @@ export const DialogDatePanelInner: FC<{
         )}
         {!!data?.data && (
           <>
-            <CalendarDateComponent data={data.data} refreshDate={refreshDate} />
+            <DiaryEntry
+              //
+              data={data.data}
+              refreshDate={refreshDate}
+            />
           </>
         )}
       </div>
     </GenericDialog>
-  )
-}
-
-const CalendarDateComponent: FC<{
-  data: TCalendarDate
-  refreshDate: () => void
-}> = ({ data, refreshDate }) => {
-  return (
-    <>
-      <p>
-        <Badge>Calendario</Badge> {data.calendar.name}
-      </p>
-      <p>
-        <Badge>Data</Badge> {displayDateFromLocalDate(data.date.date)}
-      </p>
-      {data.calendar.usesNotes && (
-        <>
-          <CalendarDateNotesComponent
-            calendarDate={data}
-            refreshDate={refreshDate}
-          />
-        </>
-      )}
-    </>
-  )
-}
-
-const CalendarDateNotesComponent: FC<{
-  calendarDate: TCalendarDate
-  refreshDate: () => void
-}> = ({ calendarDate, refreshDate }) => {
-  return (
-    <>
-      <>
-        <p>
-          <Badge>Note</Badge>
-        </p>
-        <div>
-          {calendarDate.date.notes ? (
-            <>
-              <CalendarDayNoteSeeAndEdit
-                calendarId={calendarDate.calendar.id}
-                localDate={calendarDate.date.date}
-                notes={{
-                  text: calendarDate.date.notes,
-                }}
-                editable={true} // Notes are always editable.
-                onUpdated={() => {
-                  refreshDate()
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <NotesAddForm
-                calendarId={calendarDate.calendar.id}
-                localDate={calendarDate.date.date}
-                onInserted={() => {
-                  refreshDate()
-                }}
-              />
-            </>
-          )}
-        </div>
-      </>
-    </>
-  )
-}
-
-const CalendarDayNoteSeeAndEdit: FC<{
-  calendarId: number
-  localDate: string
-  notes: {
-    text: string
-  }
-  editable: boolean
-  onUpdated: () => void
-}> = ({ calendarId, localDate, notes, editable, onUpdated }) => {
-  const { updateCalendarDateNotes } = getSDK()
-  const [editing, setEditing] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  return (
-    <div>
-      {editing ? (
-        <>
-          <NotesForm
-            initialValue={notes.text}
-            loading={loading}
-            editable={editable}
-            onSubmit={_notes => {
-              if (!!_notes && _notes === notes.text.trim()) {
-                // Useless to save: fake the update.
-                setEditing(false)
-                return
-              }
-              setLoading(true)
-              updateCalendarDateNotes(
-                calendarId,
-                localDate,
-                _notes || undefined
-              )
-                .then(() => {
-                  // Yay!
-
-                  fireEventCalendarUpdated({ calendarId })
-                  // fireEventStreamlineUpdated(undefined)
-                  // This wasn't a Planned Event.
-
-                  setLoading(false)
-                  setEditing(false)
-                  onUpdated()
-                })
-                .catch(err => {
-                  console.error(err)
-                  // TODO: Tell user all went KO
-                  alert('Errore avvenuto')
-                  setLoading(false)
-                })
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <p>
-            {notes.text}
-            {editable && (
-              <button
-                className='btn-primary ml-2'
-                onClick={() => {
-                  if (editable) {
-                    setEditing(true)
-                  }
-                }}
-              >
-                {'Edit'}
-              </button>
-            )}
-          </p>
-        </>
-      )}
-    </div>
-  )
-}
-
-const NotesAddForm: FC<{
-  calendarId: number
-  localDate: string
-  onInserted: () => void
-}> = ({ calendarId, localDate, onInserted }) => {
-  const { updateCalendarDateNotes } = getSDK()
-  const [adding, setAdding] = useState(false)
-  const [loading, setLoading] = useState(false)
-  return (
-    <>
-      {adding ? (
-        <>
-          <NotesForm
-            initialValue=''
-            loading={loading}
-            editable
-            onSubmit={_notes => {
-              setLoading(true)
-              updateCalendarDateNotes(
-                calendarId,
-                localDate,
-                _notes || undefined
-              )
-                .then(() => {
-                  // Yay!
-
-                  fireEventCalendarUpdated({ calendarId })
-                  // fireEventStreamlineUpdated(undefined)
-                  // This wasn't a Planned Event.
-
-                  setLoading(false)
-                  setAdding(false)
-                  onInserted()
-                })
-                .catch(err => {
-                  console.error(err)
-                  // TODO: Tell user all went KO
-                  alert('Errore avvenuto')
-                  setLoading(false)
-                })
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <button
-            className='btn-primary'
-            onClick={() => {
-              setAdding(true)
-            }}
-          >
-            {'Add'}
-          </button>
-        </>
-      )}
-    </>
-  )
-}
-
-const NotesForm: FC<{
-  initialValue: string
-  loading: boolean
-  editable: boolean
-  onSubmit: (value: string | null) => void
-}> = ({ initialValue, loading, editable, onSubmit }) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  return (
-    <>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-        }}
-      >
-        <input
-          type='text'
-          name='notes-text'
-          ref={inputRef}
-          className='block w-full px-2 py-1  border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring-blue-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:focus:border-blue-400 dark:focus:ring-blue-900 bg-white text-gray-700'
-          defaultValue={initialValue}
-          disabled={loading}
-        />
-        <button
-          className='btn-primary mt-1'
-          disabled={loading}
-          onClick={() => {
-            if (editable && !loading) {
-              const inputElement = inputRef.current
-              if (!inputElement) {
-                return
-              }
-              const inputValue = inputElement.value
-              if (inputValue === '') {
-                // Remove notes
-                onSubmit(null)
-              } else {
-                // TODO: Duplicated code (*sdcn)
-                const _notes = inputValue.trim()
-                if (_notes.length < 2 || _notes.length > 300) {
-                  alert('Nota non valida: minimo 2 massimo 300 caratteri')
-                  return
-                }
-                onSubmit(_notes)
-              }
-            }
-          }}
-        >
-          {'Save'}
-        </button>
-      </form>
-    </>
   )
 }
