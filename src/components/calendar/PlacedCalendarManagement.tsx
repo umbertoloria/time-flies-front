9a817a53cb1ec2531f7cdf4a0ca1d8c13d177c1e
Umberto Loria <umberto.loria@gmail.com>
2025-07-20 17:47:29 +0200

    Tabs list added, Tab History and Tab Todos added

--- src/components/calendar/PlacedCalendarManagement.tsx
+++ src/components/calendar/PlacedCalendarManagement.tsx
@@ -1,9 +1,14 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useDialogForDatePanel } from '../../context/dialog-date-panel/ContextDialogForDatePanel.tsx'
 import { CalendarLoaderComponent } from '../../context/dialog-date-panel/DialogDatePanel.tsx'
+import classNames from 'classnames'
+
+const TAB_HISTORY = 'tab-history'
+const TAB_TODOS = 'tab-todos'
 
 export const PlacedCalendarManagement: FC = () => {
   const { isOpen, data, closeDialog } = useDialogForDatePanel()
+  const [tab, setTab] = useState(TAB_TODOS)
 
   if (!isOpen || data?.mode !== 'calendar-panel') {
     return (
@@ -17,15 +22,55 @@ export const PlacedCalendarManagement: FC = () => {
     <>
       <div className='placed-calendar-management'>
         <button
-          className='btn-primary mb-2'
+          className='close-btn'
           onClick={() => {
             closeDialog()
           }}
         >
           {'Close X'}
         </button>
-        <CalendarLoaderComponent calendarId={data.calendarId} />
+        <TabsList tab={tab} setTab={setTab} />
+        <div className='tabs-content'>
+          {tab === TAB_HISTORY && (
+            <CalendarLoaderComponent calendarId={data.calendarId} />
+          )}
+          {tab === TAB_TODOS && <TabTodos />}
+        </div>
       </div>
     </>
   )
+}
+
+const TabsList: FC<{
+  tab: string
+  setTab: (tab: string) => void
+}> = ({ tab, setTab }) => {
+  return (
+    <div className='tabs-list'>
+      <button
+        className={classNames('tab-btn', {
+          active: tab === TAB_TODOS,
+        })}
+        onClick={() => {
+          setTab(TAB_TODOS)
+        }}
+      >
+        {'Todos'}
+      </button>
+      <button
+        className={classNames('tab-btn', {
+          active: tab === TAB_HISTORY,
+        })}
+        onClick={() => {
+          setTab(TAB_HISTORY)
+        }}
+      >
+        {'History'}
+      </button>
+    </div>
+  )
+}
+
+const TabTodos = () => {
+  return <div className='todos'>...</div>
 }
