c0f8780d7cc79453c4b63149db2459b9e85513f4
Umberto Loria <umberto.loria@gmail.com>
2026-05-24 20:40:05 +0200

    fix: using Next.js "standalone" export output mode

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
     API_ENDPOINT: 'https://tfapi.umbertoloria.com:8443',
   },
   /* config options here */
-  output: "export",
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
 
